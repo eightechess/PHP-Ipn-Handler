202fc442c2dca8f01e009a43cabc5dec1c901228
Jorj T <gmtebs@gmail.com>
2019-05-20 00:59:56 +0100

    Update myHandler.php

--- myHandler.php
+++ myHandler.php
@@ -15,16 +15,16 @@
     
     curl_setopt_array($ch,array(
         //Sandbox
-        CURLOPT_URL => 'https://sandbox.tranzcore.com/process',
+        CURLOPT_URL => 'https://sandbox.tranzcore.com/verify',
         //Live environment
-        //CURLOPT_URL => 'https://secure.tranzcore.com/checkout',
+        //CURLOPT_URL => 'https://secure.tranzcore.com/verify',
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_SSL_VERIFYPEER => false,
-        CURLOPT_POST => true,
-        CURLOPT_POSTFIELDS => $postFields
+        CURLOPT_POST => true,   
+        CURLOPT_POSTFIELDS => $postFields 
     ));
 
-   
+    
     $response = curl_exec($ch);
     curl_close($ch);
 
@@ -32,15 +32,17 @@
         //Update record here
 
     }
-    else{
-        //Response will be stored in file test.txt 
-        //Check file for error message
-        $handle = fopen("test.txt", "w");
-        foreach ($_POST as $key => $value)
-           fwrite($handle, "$key => $value \r\n");
-           fclose($handle);
+    // else{
+    //     //posted fields will be stored in file test.txt 
+    //     $handle = fopen("postedfields.txt", "w");
+    //     foreach ($_POST as $key => $value)
+    //        fwrite($handle, "$key => $value \r\n");
+    //        fclose($handle);
 
-    }
-
+    //     //posted fields will be stored in file response.txt
+    //        $handle = fopen("response.txt", "w");
+    //           fwrite($handle, $response);
+    //           fclose($handle);
+    // };
 
 ?>
